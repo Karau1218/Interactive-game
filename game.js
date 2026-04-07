d070175fa01f71731659026be660c3dc990a36c2
Azeb Tesfay <azbseb@gmail.com>
2026-04-07 11:12:28 -0700

    Upgrades

--- game.js
+++ game.js
@@ -1,12 +1,19 @@
-let score = 0;
-let pointsPerClick = 1;
+let score = 0
+let pointsPerClick = 1
 
 function updateDisplay() {
-  document.getElementById('score-display').textContent = 'Score: ' + score;
-  document.getElementById('rate-display').textContent = 'Points per click: ' + pointsPerClick;
+ document.getElementById('score-display').textContent = 'Score: ' + score
+ document.getElementById('rate-display').textContent = 'Points per click: ' + pointsPerClick
 }
 
-document.getElementById('click-btn').addEventListener('click', function() {
-  score += pointsPerClick;
-  updateDisplay();
-});
+document.getElementById('click-btn').addEventListener('click', function () {
+ score += pointsPerClick
+ updateDisplay()
+})
+
+const upgrades = [
+
+ { id: 1, name: "Game 1", cost: 1, bonus: 4 },
+ { id: 2, name: "Game 2", cost: 5, bonus: 6 },
+ { id: 3, name: "Game 3", cost: 5, bonus: 8 },
+]
