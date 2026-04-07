5a04f6576fd06f81aac9dcc15ede36690cc115ce
Azeb Tesfay <azbseb@gmail.com>
2026-04-07 11:32:45 -0700

    Buy an Upgrade

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-let score = 0;
-let pointsPerClick = 1;
+let score = 0
+let pointsPerClick = 1
 
 function updateDisplay() {
  document.getElementById('score-display').textContent = 'Score: ' + score
@@ -18,18 +18,30 @@ const upgrades = [
 ]
 
 function renderUpgrades() {
-  const container = document.getElementById('upgrades');
-  container.innerHTML = '';
+ const container = document.getElementById('upgrades')
+ container.innerHTML = ''
 
-  upgrades.forEach(upgrade => {
-    const div = document.createElement('div');
-    div.innerHTML = `
+ upgrades.forEach(upgrade => {
+  const div = document.createElement('div')
+  div.innerHTML = `
       <strong>${upgrade.name}</strong>
       Cost: ${upgrade.cost} | +${upgrade.bonus} per click
       <button onclick="buyUpgrade(${upgrade.id})">Buy</button>
-    `;
-    container.appendChild(div);
-  });
+    `
+  container.appendChild(div)
+ })
 }
 
-renderUpgrades();
+renderUpgrades()
+
+function buyUpgrade(id) {
+
+ const upgrade = upgrades.find(e => e.id === id)
+ if (score >= upgrades.cost) {
+  score -= upgrade.cost
+  pointsPerClick += upgrade.bonus
+  updateDisplay()
+  renderUpgrades()
+ }
+
+}
